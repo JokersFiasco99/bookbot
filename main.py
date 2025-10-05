09f31216635eb9ed8546acc533656dbbb3ca4314
JokersFiasco99 <sozinho.gil@gmail.com>
2025-10-05 13:54:49 +0200

    Update main.py logic; add .gitignore

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+
+    with open(path_to_file) as f:
+        return f.read()
+
+def count_words(text):
+    
+    return len(text.split())
+    
+
+def main():
+
+    text = get_book_text("books/frankenstein.txt")
+    print(f"Found {count_words(text)} total words")
+
+    
+
+if __name__ == "__main__":
+    main()
