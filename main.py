69b24833a395c64cddcfb08ed41bc61ba77a26d4
JokersFiasco99 <sozinho.gil@gmail.com>
2025-10-08 18:30:38 +0200

    stats: return sorted list from dict_printer; format report printing in main (reverted by user)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_characters
+from stats import get_num_words, get_num_characters, dict_printer
 
 
 def get_book_text(path_to_file):
@@ -8,10 +8,25 @@ def get_book_text(path_to_file):
 
 def main():
 
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+
     text = get_book_text("books/frankenstein.txt")
     num_characters = get_num_characters(text)
+
+    print("----------- Word Count ----------")
+
     print(f"Found {get_num_words(text)} total words")
-    print(num_characters)
+    print("--------- Character Count -------")
+
+    sorted_list = dict_printer(num_characters)
+    for item in sorted_list:
+        if not item["char"].isalpha():
+            continue
+        else:
+            print(f"{item["char"]}: {item["num"]}")
+    
+    print("============= END ===============")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -13,3 +13,17 @@ def get_num_characters(text):
         else:
             char_dict[k] = 1
     return char_dict
+
+def sort_on(single_dict):
+    return single_dict["num"]
+
+def dict_printer(new_dict):
+    new_list = []
+
+    for key, value in new_dict.items():
+        single_dict = {}
+        single_dict["char"] = key
+        single_dict["num"] = value
+        new_list.append(single_dict)
+    new_list.sort(reverse=True, key=sort_on)
+    return new_list
