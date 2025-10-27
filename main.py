7d309ac0f2424b229439c8d8e5c457dc9aec0f18
JokersFiasco99 <sozinho.gil@gmail.com>
2025-10-27 11:15:29 +0100

    finished project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import get_num_words, get_num_characters, dict_printer
-
+import sys
 
 def get_book_text(path_to_file):
 
@@ -8,10 +8,13 @@ def get_book_text(path_to_file):
 
 def main():
 
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    if sys.argv != 2:
+        print("Usage: python3 main.py <path_to_book>")
 
-    text = get_book_text("books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}...")
+
+    text = get_book_text(sys.argv[1])
     num_characters = get_num_characters(text)
 
     print("----------- Word Count ----------")
